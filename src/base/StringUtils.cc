8e4e4b971c65a734c14c5076b4ec969109956a97
xiaoenzhang <11019236+xiaoenzhang@user.noreply.gitee.com>
2025-02-18 09:38:24 +0800

    Base StringUtils

---
+++ src/base/StringUtils.cc
@@ -0,0 +1,91 @@
+#include "StringUtils.h"
+using namespace tmms::base;
+bool StringUtils::StartsWith(const std::string &s, const std::string &sub)  {
+  if(sub.empty())return true;
+  if(s.empty())return false;
+  auto len = s.size();
+  auto slen = sub.size();
+  if(slen > len)return false;
+	return s.compare(0, slen, sub) == 0;
+}
+ 
+bool StringUtils::EndsWith(const std::string &s, const std::string &sub)  {
+  if(sub.empty())return true;
+  if(s.empty())return false;
+  auto len = s.size();
+  auto slen = sub.size();
+  if(slen > len)return false;
+	return s.compare(len - slen, slen, sub) == 0;
+}
+ 
+std::string StringUtils::FilePath(const std::string &path)  {
+  //从后往前找，如果找到path中的任何一个字符，则返回其在str中的索引值.否则返回npos
+  auto pos = path.find_last_of("/\\");
+  if(pos != std::string::npos)
+  {
+    return path.substr(0, pos);
+  }else{
+    return "./";
+  } 
+}
+ 
+std::string StringUtils::FileNameExt(const std::string &path)  {
+  auto pos = path.find_last_of("/\\");
+  if(pos != std::string::npos)
+  {
+    if(pos + 1 < path.size())
+    {
+      return path.substr(pos + 1);
+    }
+  }
+  return path;
+}
+ 
+std::string StringUtils::FileName(const std::string &path)  {
+  std::string file_name = FileNameExt(path);
+  auto pos = file_name.find_last_of(".");
+  if(pos != std::string::npos)
+  {
+    //如果第一个是.的话，不认为是文件名
+    if(pos != 0)
+      return file_name.substr(0, pos);
+  }
+  return file_name;
+}
+ 
+std::string StringUtils::Extension(const std::string &path)  {
+  std::string file_name = FileNameExt(path);
+  auto pos = file_name.find_last_of(".");
+  if(pos != std::string::npos)
+  {
+    //查找不到点或者点后面没有字符
+    if(pos != 0 && pos + 1 < file_name.size())
+      return file_name.substr(pos + 1);
+  }
+  return file_name;
+
+	return std::string();
+}
+ 
+std::vector<std::string> StringUtils::SplitString(const std::string &s, const std::string &delim)  {
+  if(delim.empty())
+  {
+    return std::vector<std::string>();
+  }
+  std::vector<std::string> result;
+  size_t last = 0;
+  size_t next = 0;
+  while((next = s.find(delim, last)) != std::string::npos)
+  {
+    if(next > last)
+    {
+      result.emplace_back(s.substr(last, next - last));
+    }
+    last = next + delim.size();
+  }
+  if(last < s.size())
+  {
+    result.emplace_back(s.substr(last));
+  }
+  return result;
+}
---
+++ src/base/StringUtils.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <vector>
+namespace tmms
+{
+  namespace base
+  {
+    class StringUtils
+    {
+    public:
+      //判断字符串是否以某个字符串开头
+      static bool StartsWith(const std::string &str, const std::string &prefix);
+      //判断字符串是否以某个字符串结尾
+      static bool EndsWith(const std::string &str, const std::string &suffix);
+      //获取文件路径
+      static std::string FilePath(const std::string &path);
+      //获取文件名和扩展名
+      static std::string FileNameExt(const std::string &path);
+      //获取文件名
+      static std::string FileName(const std::string &path);
+      //获取文件扩展名
+      static std::string Extension(const std::string &path);
+      // 分割字符串
+      static std::vector<std::string> SplitString(const std::string &str, const std::string &delim);
+    };
+  }
+}
